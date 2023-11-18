cfdb58aba8aa6ae39189de1fd30eb8df098c7635
Shubham Mapari <36085347+coderastic@users.noreply.github.com>
2023-11-18 12:47:12 -0800

    make it better i guess

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from flask import Flask, render_template, request
 from pytrends.request import TrendReq
-import os
 
 app = Flask(__name__)
 
-def get_google_trends_keywords(keyword):
+def get_google_trends_data(keyword, timeframe, geo):
     pytrends = TrendReq(hl='en-US', tz=360)
-    pytrends.build_payload([keyword], cat=0, timeframe='today 12-m', geo='', gprop='')
+    pytrends.build_payload([keyword], cat=0, timeframe=timeframe, geo=geo, gprop='')
     related_queries = pytrends.related_queries()
-    return related_queries[keyword]['top']['query'].tolist() if related_queries.get(keyword) else []
+    suggestions = pytrends.suggestions(keyword)
+
+    data = {
+        'trends': related_queries[keyword]['top']['query'].tolist() if related_queries.get(keyword) else [],
+        'suggestions': [suggestion['title'] for suggestion in suggestions]
+    }
+    return data
 
 @app.route('/')
 def index():
@@ -17,10 +22,13 @@ def index():
 @app.route('/find-trends', methods=['POST'])
 def find_trends():
     keyword = request.form['keyword']
-    trends_keywords = get_google_trends_keywords(keyword)
-    return render_template('results.html', keyword=keyword, trends=trends_keywords)
+    timeframe = request.form.get('timeframe', 'today 12-m')
+    geo = request.form.get('geo', '')
+    trends_data = get_google_trends_data(keyword, timeframe, geo)
+    
+    # Implement pagination logic here if necessary
+
+    return render_template('results.html', keyword=keyword, trends_data=trends_data)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
-
+    app.run(debug=True)
