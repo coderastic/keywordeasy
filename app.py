267d6aa5475c8a838d4d1d2e7c13cbc993fd0649
Shubham Mapari <36085347+coderastic@users.noreply.github.com>
2023-11-18 12:53:02 -0800

    ok again

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from pytrends.request import TrendReq
+import os
 
 app = Flask(__name__)
 
@@ -31,4 +32,5 @@ def find_trends():
     return render_template('results.html', keyword=keyword, trends_data=trends_data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
