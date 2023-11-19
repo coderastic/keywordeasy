e2a50cc79a8711623a84a9b801823159a83f6cb4
Shubham Mapari <36085347+coderastic@users.noreply.github.com>
2023-11-19 10:38:59 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,11 @@
 from flask import Flask, render_template, request
 from pytrends.request import TrendReq
-import requests
 import os
 
 app = Flask(__name__)
-live_proxies = []
-
-def is_proxy_live(proxy):
-    try:
-        test_url = 'http://www.google.com'
-        response = requests.get(test_url, proxies={'https': proxy}, timeout=5)
-        return response.status_code == 200
-    except requests.RequestException:
-        return False
-
-def fetch_and_test_proxies():
-    api_url = 'https://api.proxyscrape.com/v2/?request=displayproxies&protocol=https&timeout=250&country=all&ssl=all&anonymity=all'
-    response = requests.get(api_url)
-    proxy_list = response.text.strip().split('\n')
-    live_proxies.clear()
-
-    for proxy in proxy_list:
-        formatted_proxy = f'https://{proxy}'
-        if is_proxy_live(formatted_proxy) and len(live_proxies) < 5:
-            live_proxies.append(formatted_proxy)
-
-@app.before_first_request
-def initialize_proxies():
-    fetch_and_test_proxies()
 
 def get_google_trends_data(keyword, timeframe, geo):
-    pytrends = TrendReq(hl='en-US', tz=360, proxies=live_proxies)
+    pytrends = TrendReq(hl='en-US', tz=360)
     try:
         pytrends.build_payload([keyword], cat=0, timeframe=timeframe, geo=geo if geo else '', gprop='')
         related_queries = pytrends.related_queries()
@@ -68,4 +43,4 @@ def find_trends():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
-# over
+
