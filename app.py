5c19a10176459834adb77672dc00d461d0372d84
Shubham Mapari <36085347+coderastic@users.noreply.github.com>
2023-11-19 07:13:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 from flask import Flask, render_template, request
 from pytrends.request import TrendReq
-from pytrends.exceptions import ResponseError
-import os
+import requests
+import random
 
 app = Flask(__name__)
 
-def get_google_trends_data(keyword, timeframe, geo):
-    pytrends = TrendReq(hl='en-US', tz=360)
+def is_proxy_live(proxy):
+    try:
+        test_url = 'http://www.google.com'
+        response = requests.get(test_url, proxies={'https': proxy}, timeout=5)
+        return response.status_code == 200
+    except requests.RequestException:
+        return False
+
+def get_live_proxies():
+    api_url = 'https://api.proxyscrape.com/v2/?request=displayproxies&protocol=http&timeout=250&country=all&ssl=all&anonymity=all'
+    response = requests.get(api_url)
+    proxy_list = response.text.strip().split('\n')
+    return [proxy for proxy in proxy_list if is_proxy_live(proxy)]
+
+def get_google_trends_data(keyword, timeframe, geo, proxies):
+    proxy = {'https': random.choice(proxies)} if proxies else None
+    pytrends = TrendReq(hl='en-US', tz=360, proxies=proxy)
     try:
         pytrends.build_payload([keyword], cat=0, timeframe=timeframe, geo=geo if geo else '', gprop='')
         related_queries = pytrends.related_queries()
@@ -21,10 +36,8 @@ def get_google_trends_data(keyword, timeframe, geo):
             'suggestions': [suggestion['title'] for suggestion in suggestions]
         }
         return data
-    except ResponseError as e:
-        if e.response and e.response.status_code == 429:
-            return {'trends': [], 'suggestions': [], 'error': 'Too many requests. Please try again later.'}
-        raise
+    except Exception as e:
+        return {'trends': [], 'suggestions': [], 'error': str(e)}
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -35,21 +48,15 @@ def find_trends():
     keyword = request.args.get('keyword') if request.method == 'GET' else request.form['keyword']
     timeframe = request.args.get('timeframe', 'today 12-m') if request.method == 'GET' else request.form.get('timeframe', 'today 12-m')
     geo = request.args.get('geo', '') if request.method == 'GET' else request.form.get('geo', '')
-    page = request.args.get('page', 1, type=int)
+    proxies = get_live_proxies()
 
-    trends_data = get_google_trends_data(keyword, timeframe, geo)
+    trends_data = get_google_trends_data(keyword, timeframe, geo, proxies)
     if 'error' in trends_data:
         return render_template('error.html', error=trends_data['error'])
 
-    # Pagination Logic
-    per_page = 20
-    total_keywords = len(trends_data['trends'])
-    start = (page - 1) * per_page
-    end = start + per_page
-    paginated_keywords = trends_data['trends'][start:end]
-    total_pages = (total_keywords - 1) // per_page + 1
+    # (Add your pagination logic here)
 
-    return render_template('results.html', keyword=keyword, trends_data=trends_data, page=page, total_pages=total_pages, suggestions=trends_data['suggestions'])
+    return render_template('results.html', keyword=keyword, trends_data=trends_data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
